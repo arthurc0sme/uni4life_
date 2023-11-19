76c34428a3ef4ce088f52d0986900e8c0499dd17
victor-red <victor_ribeiro01@outlook.com>
2023-11-19 00:36:42 -0300

    Versão inicial do front-end da tela de login.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,62 +1,54 @@
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/authContext";
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 import "./login.scss";
+import axios from "axios";
+
 
 const Login = () => {
-  
+
   const [inputs, setInputs] = useState({
     username:"",
+    email:"",
     password:"",
+    name:"",
   })
 
   const [err, setErr] = useState(null);
-
-  const navigate = useNavigate()
 
   const handleChange = (e) =>{
     setInputs(prev=>({...prev, [e.target.name]: e.target.value }))
   };
 
-  const { login } = useContext(AuthContext);
-
-  const handleLogin =  async(e) => {
+  const handleClick = async (e) =>{
     e.preventDefault()
-    try {
-      await login(inputs);
-      navigate("/")
-    } catch (err){
-      setErr(err.response.data)
+
+    try{
+        await axios.post("http://localhost:8800/api/auth/login/", inputs);
+    } catch(err){
+      setErr(err.response.data);
     }
-    
+
   };
 
   return (
     <div className="login">
       <div className="card">
-        <div className="left">
-          <h1>Bem Vindo!</h1>
-          <p>
-            Faça login para estar por dentro de tudo que acontece na sua universidade!
-            E Fique por dentro das novidades do seu mundo acadêmico!
-          </p>
-          <span>Você ainda não possui uma conta?</span>
-          <Link to="/register">
-            <button>Registre-se</button>
-          </Link>
-        </div>
         <div className="right">
-          <h1>Login</h1>
+          <h1 className='center'>Uni4Life</h1>
           <form>
-            <input type="text" placeholder="Nome de Usuário" name="username" onChange={handleChange} />
-            <input type="password" placeholder="Senha" name="password" onChange={handleChange} />
+            <input type="email" placeholder="Email" name="email" onChange={handleChange}  />
+            <input type="password" placeholder="Senha" name="password" onChange={handleChange}  />
+            
             {err && err}
-            <button onClick={handleLogin}>Login</button>
-          </form>
+
+            <button id='center' onClick={handleClick}>Login</button>
+            <p className='toRegister'>
+               <Link to="/register" style={{color: '#666'}}>Não tem uma conta?</Link>
+            </p>
+            </form>
         </div>
       </div>
     </div>
   );
 };
-
 export default Login;
